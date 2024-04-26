a3d2378244e02b9d3cf6749b9e89a5f792fe4f42
ayoubzouine94 <ayoubzouine94@gmail.com>
2024-04-26 19:36:06 +0100

    Initial Commit

---
+++ YassirTest/YassirTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YassirTestApp.swift
+//  YassirTest
+//
+//  Created by Ayoub on 26/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct YassirTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
